5d071635575aa2f5eabc28bfd62590aad5aacbea
Ian Rose <ian@ianrose.co>
2014-03-02 14:37:51 -0500

    set csslint to lint css only

--- Gruntfile.js
+++ Gruntfile.js
@@ -315,8 +315,8 @@ module.exports = function (grunt) {
       },
       check: {
         src: [
-          '<%= yeoman.app %>/css/**/*.css',
-          '<%= yeoman.app %>/_scss/**/*.scss'
+          '<%= yeoman.app %>/css/**/*.css'
+//          '<%= yeoman.app %>/_scss/**/*.scss'
         ]
       }
     },
